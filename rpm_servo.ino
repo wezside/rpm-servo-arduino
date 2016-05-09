dce32c9efdb8e5101e551e878b90d2c2b31c88f7
wezside <wesley@wezside.co.za>
2016-05-09 23:19:25 +0200

    Added the back sweep

--- rpm_servo.ino
+++ rpm_servo.ino
@@ -35,6 +35,12 @@ void sweep()
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
+  delay(1000);
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
 }
  
 void loop() 
